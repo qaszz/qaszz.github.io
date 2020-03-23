306f33a9f9ac1cc99d85507c36ffa6f61bfdce26
qaszz <32517805+qaszz@users.noreply.github.com>
2020-03-23 19:26:25 +0100

    javascript 2/3 ferdig

--- index.js
+++ index.js
@@ -12,16 +12,20 @@ function addNewWorker(event){
     renderWorkerList();
 
     event.target.reset();
-    console.log(worker);
 }
 function renderWorkerList(){
     const workerList = JSON.parse(window.localStorage.getItem("workerList")) || [];
     const workerListEl = document.getElementById("workerListUl");
+    const workerDropDown = document.getElementById("workerDropDown")
     workerListEl.innerHTML = "";
+    workerDropDown.innerHTML = "";
     for (const worker of workerList){
         const workerEl = document.createElement("li");
-        workerEl.innerHTML = `<li>${worker.name}</li>`
-        workerListEl.appendChild(workerEl)
+        const workerDrop = document.createElement("option");
+        workerEl.innerHTML = `<li>${worker.name}</li>`;
+        workerDrop.innerHTML = `<option>${worker.name}</option>`;
+        workerListEl.appendChild(workerEl);
+        workerDropDown.appendChild(workerDrop);
     }
 }
 
@@ -34,3 +38,39 @@ window.addEventListener("storage", function(event){
 renderWorkerList();
 
 /* ARBEIDSOPPGAVERELATERTE FUNKSJONER */
+function addNewTask(event){
+    event.preventDefault();
+    const task = document.querySelector("[name='task']").value;
+
+    const work = {task};
+
+    const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
+    taskList.push(work);
+    window.localStorage.setItem("taskList", JSON.stringify(taskList));
+    renderTaskList()
+
+    event.target.reset()
+    console.log(work);
+}
+
+function renderTaskList(){
+    const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
+    const taskListEl = document.getElementById("taskListUl");
+    const taskDropDown = document.getElementById("taskDropDown")
+    taskListUl.innerHTML = "";
+    taskDropDown.innerHTML = "";
+    for (const work of taskList){
+        const taskEl = document.createElement("li");
+        const taskDrop = document.createElement("option");
+        taskEl.innerHTML = `<li>${work.task}</li>`;
+        taskDrop.innerHTML = `<option>${work.task}</option>`;
+        taskListEl.appendChild(taskEl);
+        taskDropDown.appendChild(taskDrop);
+    }   
+    window.addEventListener("storage", function(event){
+        if (event.key === "taskList"){
+            renderTaskList();
+        }
+    })
+}
+renderTaskList();
